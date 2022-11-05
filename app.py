8c404a582eadc693e5dc6ff64fedd972dab05282
Pato TB <patriciotubio@gmail.com>
2022-11-04 23:46:25 -0300

    form basic with POST

--- app.py
+++ app.py
@@ -2,11 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "basketball",
+    "football",
+    "voleyball"
+]
+
+database = {
+
+}
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/greet", methods=["GET"])
 def greet():
     name = request.args.get("name")
-    return render_template("greet.html", name=name)
+    return render_template("greet.html", name=name, sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():   
+  if not request.form.get("name"):
+      return render_template("failure.html", message="Missing name :(")
+  if not request.form.get("sport"):
+      return render_template("failure.html", message="Missing sport :(")
+  if request.form.get("sport") not in SPORTS:
+      return render_template("failure.html", message="Invalid sport :(")
+  return render_template("success.html")
